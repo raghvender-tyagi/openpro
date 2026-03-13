27616e8f4bfa8fa4fa83a3a0d590c3eba42fb89b
raghvender-tyagi <sunilkumartyagi900@gmail.com>
2026-03-14 00:52:24 +0530

    hey #37

--- app/layout.js
+++ app/layout.js
@@ -5,7 +5,7 @@ const inter = Inter({
   subsets: ["latin"],
   variable: "--font-sans",
 });
-
+//setgsredgrthj
 const jetbrainsMono = JetBrains_Mono({
   subsets: ["latin"],
   variable: "--font-mono",
